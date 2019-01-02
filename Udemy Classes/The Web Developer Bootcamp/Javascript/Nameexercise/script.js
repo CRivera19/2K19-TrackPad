20eeb9df83ab1e453df605f76385ccfc9b40ce44
Ctrivera19 <ctrivera19@gmail.com>
2019-01-02 14:57:37 -0800

    two exercises

---
+++ Udemy Classes/The Web Developer Bootcamp/Javascript/Nameexercise/script.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var fullName = firstName + " " + lastName;
+var age = prompt("What is your age?");
+
+alert("Your full name is " + fullName + " and you are " + age + " years old.")
+
